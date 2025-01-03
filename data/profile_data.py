06e82f8347b89870f73b4f196f9d33cdb9fd4cb1
Andr-NG <andreynguyen467@gmail.com>
2025-01-03 14:06:39 +0700

    updating

--- data/profile_data.py
+++ data/profile_data.py
@@ -4,6 +4,7 @@ fake = faker.Faker()
 PROFILE_GENERIC = {
     'browser_type': 'mimic',
     'folder_id': '',
+    'core_version': 131,
     'name': f'{fake.name()}',
     'os_type': 'windows',
     'parameters': {
